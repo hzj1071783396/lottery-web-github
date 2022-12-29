c81216976a286fab55b1da371b87cceb7755fe78
hu <1071783396@qq.com>
2022-12-29 12:23:27 +0800

    修改打包路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"./",
   plugins: [vue(),
     Components({
       resolvers: [VantResolver()],
